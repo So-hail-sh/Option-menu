60b436d483189b9996a7c57a52e00ba7e9e70ecb
So-hail-sh <sohailsh787@gmail.com>
2021-03-02 18:30:23 +0530

    Option menu

---
+++ app/src/main/java/com/example/option/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.option;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+
+
+            case R.id.us:
+
+
+                break;
+
+
+
+                case R.id.aa:
+                    break;
+
+
+        }
+
+
+                return super.onOptionsItemSelected(item);
+            }
+        }
